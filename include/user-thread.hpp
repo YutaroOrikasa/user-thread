19fdb8616d81a99d1974dea09fc575ba96b3d89c
orikasa <orikasa@csg.ci.i.u-tokyo.ac.jp>
2017-05-05 19:08:15 +0900

    Add global worker manager and start_main_thread/start_thread function

--- include/user-thread.hpp
+++ include/user-thread.hpp
@@ -13,7 +13,7 @@
 
 namespace orks {
 namespace userthread {
-using namespace detail;
+using namespace detail; // I think this is dangerous.
 
 class Thread {
     ThreadData* thread_data;
@@ -103,16 +103,45 @@ public:
 void yield_thread();
 Thread start_thread(void (*func)(void* arg), void* arg);
 
+/*
+* initialize global worker manager with the number of the worker.
+* DO NOT call twice.
+*/
+void init_worker_manager(unsigned int number_of_worker);
+
+/*
+* initialize global worker manager with the number of the cpu cores.
+* DO NOT call twice.
+*/
+void init_worker_manager();
+
+/**
+* This function blocks until all user threads finish.
+*/
+void start_main_thread(void (*func)(void* arg), void* arg);
 }
 }
 
 
-namespace {
-// WorkQueue work_queue { 1 };
+namespace orks {
+namespace userthread {
+namespace detail {
 
+inline
+void start_main_thread(WorkerManager& wm, void (*func)(void*), void* arg) {
+    wm.start_main_thread(func, arg);
+}
 
+inline
+Thread start_thread(WorkerManager& wm, void (*func)(void*), void* arg) {
+    return wm.start_thread(func, arg);
+}
 
 }
+}
+}
+
+
 
 
 #endif /* USER_THREAD_HPP_ */
