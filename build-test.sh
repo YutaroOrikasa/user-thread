e98a3b08c64787125176d6045fbd79d8d413a71f
orikasa <orikasa@csg.ci.i.u-tokyo.ac.jp>
2017-05-20 18:34:16 +0900

    Remove unused build scripts

--- build-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xv
-
-cd $(dirname "$0")
-
-GTEST_DIR=googletest/googletest
-
-g++ -std=gnu++14 -isystem ${GTEST_DIR}/include -I./include -pthread test/test.cpp libuser-thread.a libgtest.a -o test_
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xve
-
-cd src
-
-g++ -std=c++14 -I../include mysetjmp.s user-thread.cpp -lpthread -O3 -c
-
-ar rcs ../libuser-thread.a mysetjmp.o user-thread.o
