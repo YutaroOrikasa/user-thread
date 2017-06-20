1fd553e57ae7448a38284934be5bf4016efb7113
orikasa <orikasa@csg.ci.i.u-tokyo.ac.jp>
2017-06-20 18:29:16 +0900

    Fix unittest failiure when main thread finishes before child threads finishing

--- test/test.cpp
+++ test/test.cpp
@@ -11,17 +11,24 @@ struct Args {
     WorkerManager& wm;
     const int thread_size;
     std::atomic_int& counter;
+    std::atomic_int& alive_thread_counter;
 };
 void child_thread_for_test(void* arg) {
     printf("child thread\n");
-    std::atomic_int& counter = *reinterpret_cast<std::atomic_int*>(arg);
-    ++counter;
+    Args& args = *reinterpret_cast<Args*>(arg);
+
+    ++args.counter;
+
+    --args.alive_thread_counter;
 }
 void main_thread_for_test(void* arg) {
     printf("main thread\n");
     Args& args = *reinterpret_cast<Args*>(arg);
     for (int i = 0; i < args.thread_size; ++i) {
-        args.wm.start_thread(child_thread_for_test, &args.counter);
+        args.wm.start_thread(child_thread_for_test, &args);
+    }
+    while (args.alive_thread_counter != 0) {
+        args.wm.scheduling_yield();
     }
 }
 
@@ -33,12 +40,17 @@ void child_thread_for_test_yield(void* arg) {
     args.wm.scheduling_yield();
     ++args.counter;
 
+    --args.alive_thread_counter;
+
 }
 void main_thread_for_test_yield(void* arg) {
     printf("main thread\n");
     Args& args = *reinterpret_cast<Args*>(arg);
     for (int i = 0; i < args.thread_size; ++i) {
         args.wm.start_thread(child_thread_for_test_yield, &args);
+    }
+    while (args.alive_thread_counter != 0) {
+        args.wm.scheduling_yield();
     }
 }
 
@@ -49,7 +61,8 @@ TEST(WorkerManager, Test) {
     WorkerManager wm { 4 };
     std::atomic_int counter { 0 };
     int thread_size = 8;
-    Args args = { wm, thread_size, counter };
+    std::atomic_int alive_thread_counter {thread_size};
+    Args args = { wm, thread_size, counter, alive_thread_counter };
     wm.start_main_thread(main_thread_for_test, &args);
     ASSERT_EQ(thread_size, counter);
 }
@@ -58,8 +71,10 @@ TEST(WorkerManager, TestYield) {
 
     WorkerManager wm { 4 };
     std::atomic_int counter { 0 };
+    std::atomic_int finished_thread_counter {0};
     int thread_size = 8;
-    Args args = { wm, thread_size, counter };
+    std::atomic_int alive_thread_counter {thread_size};
+    Args args = { wm, thread_size, counter, alive_thread_counter };
     wm.start_main_thread(main_thread_for_test_yield, &args);
     ASSERT_EQ(thread_size * 2, counter);
 }
